a97dde547fc680c235c632db8218f00744d9bff3
nck <nguyencongkhue123@gmail.com>
2024-11-28 12:57:53 +0700

    tao giao dien - tao diem tren panel

--- CODE/kmean.py
+++ CODE/kmean.py
@@ -1,13 +1,16 @@
 import pygame   #dùng thư viện pygame
 from random import randint
+from sklearn.cluster import KMeans
 import math 
+import numpy as np 
+import matplotlib.pyplot as plt
 def distance(p1,p2):
     return math.sqrt((p1[0]-p2[0])*(p1[0]-p2[0]) + (p1[1]-p2[1])*(p1[1]-p2[1]))
-
 pygame.init()   #khởi tạo thư viện
 screen=pygame.display.set_mode((1200,700)) #tạo màn hình
 pygame.display.set_caption('KMean Visualition') #ghitiêuđề
 running=True
+clock =pygame.time.Clock()
 BACKGROUND=(214,214,214)
 BLACK=(0,0,0)
 WHITE=(255,255,255)
@@ -21,21 +24,24 @@ ORGANCE=(255,125,25)
 GRAPE=(100,155,65)
 GRASS=(55,155,65)
 COLORS=[RED,GREEN, BLUE, YELLOW, SKY, ORGANCE, GRAPE, GRASS]
-
 font=pygame.font.SysFont('sans',40)
+fontsmall=pygame.font.SysFont('sans',20)
 text_plus=font.render('+', True, WHITE)
 text_minus=font.render('-', True, WHITE)
 text_run=font.render('Run', True, WHITE)
 text_random=font.render('Random', True, WHITE)
 text_algorithm=font.render('Algorithm', True, WHITE)
 text_reset=font.render('Reset', True, WHITE)
+text_elbow = font.render("Elbow", True, WHITE)
 K=0
 error=0
 points=[]
 clusters=[]
 labels=[]
 while running:
+    clock.tick(60)
     screen.fill(BACKGROUND)
+    mouse_x, mouse_y=pygame.mouse.get_pos()
     #ve giao dien -Draw interface
     #ve panel
     pygame.draw.rect(screen, BLACK, (50,50,700,500))
@@ -55,6 +61,9 @@ while running:
     # Random  button
     pygame.draw.rect(screen, BLACK, (850,250,150,50))
     screen.blit(text_random, (850,250))
+    #Vẽ elbow button
+    pygame.draw.rect(screen, BLACK, (850, 450, 150, 50))
+    screen.blit(text_elbow, (850, 450))
     # Algorithm button scikit -learn
     pygame.draw.rect(screen, BLACK, (850,450,150,50))
     screen.blit(text_algorithm, (850,450))
@@ -64,9 +73,12 @@ while running:
     #text_error
     text_error=font.render('Error=' + str(int(error)), True, BLACK)
     screen.blit(text_error, (850,350))
+        # draw mouse pos when it is panel
+    if 50<mouse_x <750 and 50<mouse_y<550:
+        text_mouse=fontsmall.render('(' +str(mouse_x-50)+','+ str(mouse_y-50)+ ')',True, BLACK)
+        screen.blit(text_mouse, (mouse_x+10,mouse_y))
     #ket thuc giao dien
     mouse_x, mouse_y=pygame.mouse.get_pos()
-
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
@@ -77,8 +89,12 @@ while running:
                 points.append(point)
             #change K button + 
             if 850<mouse_x< 900 and 50<mouse_y<100:
+                 K+=1
                 print('Press K+')
+             #change K button - 
             if 950<mouse_x< 1000 and 50<mouse_y<100:
+                 if K>0:
+                    K-=1
                 print('Press -')
             # Run button
             if 850<mouse_x< 1000 and 150<mouse_y<200:
@@ -104,9 +120,6 @@ while running:
                         new_x=sum_x/count
                         new_y=sum_y/count
                         clusters[i]=(new_x, new_y)
-                                  
-              
-             
                 print('Press Run ')
             # Random button
             if 850<mouse_x< 1000 and 250<mouse_y<300:
@@ -128,10 +141,7 @@ while running:
             pygame.draw.circle(screen, WHITE, (points[i][0]+50, points[i][1]+50), 5)
         else: 
             pygame.draw.circle(screen, COLORS[labels[i]], (points[i][0]+50, points[i][1]+50), 5)  
-
-
     for i in range(len(clusters)):
         pygame.draw.circle(screen, COLORS[i], (clusters[i][0]+50, clusters[i][1]+50), 10)
-                
     pygame.display.flip() #cập nhật thay đổi màn hình
 pygame.quit()
